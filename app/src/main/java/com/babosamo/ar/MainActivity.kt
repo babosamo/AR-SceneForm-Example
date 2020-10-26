0db1230dc1c1c34a675d9437cdfa2ba239bc59ad
babosamo <babosamo@gmail.com>
2020-10-26 17:24:58 +0900

    add textView to scene

--- app/src/main/java/com/babosamo/ar/MainActivity.kt
+++ app/src/main/java/com/babosamo/ar/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.babosamo.ar
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.BaseArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import java.util.function.Consumer
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,20 +24,40 @@ class MainActivity : AppCompatActivity() {
         arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
 
         arFragment.setOnTapArPlaneListener(BaseArFragment.OnTapArPlaneListener { hitResult, plane, motionEvent ->
-            val anchor: Anchor = hitResult.createAnchor()
 
-            ModelRenderable.builder()
-                .setSource(this, Uri.parse("dic.sfb"))
+            val anchor: Anchor = hitResult.createAnchor()
+//
+//            ModelRenderable.builder()
+//                .setSource(this, Uri.parse("dic.sfb"))
+//                .build()
+//                .thenAccept { addModelToScene(anchor, it) }
+//                .exceptionally {
+//                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+//                    builder.setMessage(it.localizedMessage)
+//                        .show()
+//                    return@exceptionally null
+//                }
+
+            ViewRenderable.builder()
+                .setView(this, R.layout.layout_ar_item)
                 .build()
-                .thenAccept { addModelToScene(anchor, it) }
-                .exceptionally {
-                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-                    builder.setMessage(it.localizedMessage)
-                        .show()
-                    return@exceptionally null
-                }
+                .thenAccept(Consumer { renderable: ViewRenderable ->
+                    addViewToscnee(anchor, renderable)
+
+                })
 
         })
+    }
+
+    private fun addViewToscnee(anchor: Anchor, it: ViewRenderable?) {
+        val anchorNode = AnchorNode(anchor)
+        val transform = TransformableNode(arFragment.transformationSystem)
+//        transform.scaleController.maxScale = 0.2f
+//        transform.scaleController.minScale = 0.1f
+        transform.setParent(anchorNode)
+        transform.renderable = it
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        transform.select()
     }
 
 
